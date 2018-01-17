f1604984788d065204c783d78db36b43962488f4
Piotr Stus <Piters2s@gmail.com>
2018-01-17 20:50:51 +0100

    Convex_hull

---
+++ Otoczka_Wypukla/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Otoczka_Wypukla
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.generate_button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(88, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // generate_button
+            // 
+            this.generate_button.Location = new System.Drawing.Point(239, 418);
+            this.generate_button.Name = "generate_button";
+            this.generate_button.Size = new System.Drawing.Size(84, 38);
+            this.generate_button.TabIndex = 1;
+            this.generate_button.Text = "Generate";
+            this.generate_button.UseVisualStyleBackColor = true;
+            this.generate_button.Click += new System.EventHandler(this.generate_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 468);
+            this.Controls.Add(this.generate_button);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button generate_button;
+    }
+}
+
---
+++ Otoczka_Wypukla/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace Otoczka_Wypukla
+{
+    public partial class Form1 : Form
+    {
+
+     
+
+        private readonly Graphics _graphics;
+        Pen pen = new Pen(Color.Red, 1);
+
+        private Point[] _pointsTab;
+
+
+        private List<Point> _randomPoints = new List<Point>();
+
+        private List<Point> _vertices;
+        private List<Point> _pointsList;
+
+        int pointsQuantity = 100;
+
+
+        public Form1()
+
+        {
+            InitializeComponent();
+            _graphics = pictureBox1.CreateGraphics();
+
+
+        }
+
+        private void generate_button_Click(object sender, EventArgs e)
+        {
+            _graphics.Clear(Color.White);
+            CreatePoints();
+            FindStartPoints();
+
+            _pointsList = CountHullJarvis(_vertices[0], _vertices[1], _randomPoints);
+            _pointsList.Remove(_vertices[0]);
+
+
+
+            _pointsTab = _pointsList.ToArray();
+            _graphics.DrawLines(pen, _pointsTab);
+           
+        }
+
+        //Generate random points
+        private void CreatePoints()
+        {
+            var _random = new Random();
+
+            int size = pictureBox1.Width;
+
+            for (int i = 0; i < pointsQuantity; i++)
+            {
+                int x = _random.Next(5, size - 5);
+                int y = _random.Next(5, size - 5);
+
+                var point = new Point(x, y);
+
+                _randomPoints.Add(point);
+                _graphics.FillRectangle(new SolidBrush(Color.Black), point.X, point.Y, 1, 1);
+                _graphics.FillRectangle(new SolidBrush(Color.Black), point.X, point.Y, 1, 1);
+                //_graphics.DrawImage(img, point.X, point.Y);
+
+            }
+        }
+
+        private void FindStartPoints()
+        {
+            var P0 = new Point();
+            var P1 = new Point(0, 0); //lowest point
+
+
+            // Finding lowest point
+            foreach (var Point in _randomPoints)
+            {
+                if (Point.Y > P1.Y)
+                {
+                    P1 = Point;
+                }
+            }
+
+            P0.X = -pictureBox1.Width + 1;
+            P0.Y = P1.Y;
+
+            _graphics.FillRectangle(new SolidBrush(Color.Red), P1.X, P1.Y, 10, 10);
+     
+
+            _vertices = new List<Point>();
+            _vertices.Add(P0);
+            _vertices.Add(P1);
+        }
+
+
+
+
+        public List<Point> CountHullJarvis(Point p0, Point p1, List<Point> list)
+        {
+
+          //  Point min = p1;
+            Point p2 = new Point(0, 0);
+            List<Point> _hullVertices = new List<Point>();
+            double angle = 0;
+
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                angle = 0;
+                for (int j = 0; j < list.Count; j++)
+                {
+                    if (countAngle(p0, p1, list[j]) > angle)
+                    {
+                        p2 = list[j];
+                        angle = countAngle(p0, p1, p2);
+                    }
+                }
+
+                p0 = p1;
+                p1 = p2;
+
+                _hullVertices.Add(p2);
+            }
+
+
+            return _hullVertices;
+
+        }
+
+        //public List<Point> CountHullTriangles(List<Point> list)
+        //{
+        //    List<Point> _hullVertices = new List<Point>();
+        //    _hullVertices = list;
+        //    var _random = new Random();
+
+        //    Point p0 = new Point();
+        //    Point p1 = new Point();
+        //    Point p2 = new Point();
+        //    Point point = new Point();
+
+        //    while (true)
+        //    {
+        //        p0 = _hullVertices[_random.Next(0, _hullVertices.Count)];
+        //        p1 = _hullVertices[_random.Next(0, _hullVertices.Count)];
+        //        p2 = _hullVertices[_random.Next(0, _hullVertices.Count)];
+        //        int maxX = int.MinValue;
+
+        //        for (int i = 0; i < _hullVertices.Count; i++)
+        //        {
+        //            if (list[i].X > maxX)
+        //                maxX = _hullVertices[i].X;
+        //        }
+        //        int rx = maxX + 1;
+
+        //        int ry = point.Y;
+        //        int c = 0; // c- iloś przecięć
+        //        for (int i = 0; i < _hullVertices.Count; i++)
+        //        {
+
+        //            if (IsPart(p0.X, p0.Y, p1.X, p1.Y, p2.X, p2.Y) == 1)
+        //            {
+        //                c++;
+        //            }
+
+        //            //if (IsCrossing(p0.X, p0.Y, p1.X, p1.Y, p2.X, p2.Y, rx, ry))
+        //            //    c++;
+        //        }
+
+
+
+        //        if (c % 2 != 0)
+        //        {
+        //            //usun punkt z hullVertices
+        //        }
+        //    }
+        //    return _hullVertices;
+
+        //}
+
+
+
+
+
+        //private int IsPart(int ax, int ay, int bx, int by, int cx, int cy)
+        //{
+
+
+        //    {
+        //        int det = Det(ax, ay, bx, by, cx, cy);
+        //        if (det != 0)
+        //            return 0;
+        //        if ((Math.Min(ax, bx) <= cx) && (cx <= Math.Max(ax, bx)) &&
+        //            (Math.Min(ay, by) <= cy) && (cy <= Math.Min(ay, by)))
+        //            return 1;
+        //        return 0;
+        //    }
+
+        //   int Det(int xx, int xy, int yx, int yy, int zx, int zy)
+        //   {
+        //        return (xx * yy + yx * zy + zx * xy - zx * yy - xx * zy - yx * xy);
+        //   }
+
+
+        //    //bool IsCrossing(int ax, int ay, int bx, int by, int px, int py, int rx, int ry)
+        //    //{
+
+        //    //    return (Math.Sign(Det(px, py, rx, ry, ax, ay)) != Math.Sign(Det(px, py, rx, ry, bx, by))) &&
+        //    //           (Math.Sign(Det(ax, ay, bx, by, px, py)) != Math.Sign(Det(ax, ay, bx, by, rx, ry)));
+        //    //}
+
+        //}
+
+        public double countAngle(Point p0, Point p1, Point p2)
+        {
+            double alpha = 0, la = 0, lb = 0;
+            Point va = new Point();
+            Point vb = new Point();
+
+            va.X = p2.X - p1.X;
+            va.Y = p2.Y - p1.Y;
+            vb.X = p0.X - p1.X;
+            vb.Y = p0.Y - p1.Y;
+
+            la = Math.Sqrt(Math.Pow(va.X, 2) + Math.Pow(va.Y, 2));
+            lb = Math.Sqrt(Math.Pow(vb.X, 2) + Math.Pow(vb.Y, 2));
+
+            double scal = (va.X * vb.X + va.Y * vb.Y);
+
+            alpha = scal / (la * lb);
+            double cos = Math.Acos(alpha) * (180 / Math.PI);
+
+            return cos;
+
+
+        }
+    }
+}
